231ff240a93a686ae7bdaaabe4cfd70f1cf28b6f
SyAndrey <69247757+SyAndrey006@users.noreply.github.com>
2025-07-21 12:34:55 +0200

    Add files via upload

---
+++ Day 15/Coffee Machine Project/main.py
@@ -0,0 +1,89 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+    "money": 0
+}
+
+def print_report():
+    print(f"Water: {resources["water"]}ml")
+    print(f"Milk: {resources["milk"]}ml")
+    print(f"Coffee: {resources["coffee"]}g")
+    print(f"Money: {resources['money']}$")
+
+def can_made(order_ingredients):
+    for item in order_ingredients:
+        if order_ingredients[item] > resources[item]:
+            print(f"Sorry there is not enough {item}.")
+            return False
+    return True
+
+def count_coins():
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+def is_transaction_successful(money_received, drink_cost):
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        resources["money"] += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+
+def make_coffee(drink_name, order_ingredients):
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕️. Enjoy!")
+
+def work():
+    is_working = True
+    while is_working:
+        choice = input("What would you like? (espresso/latte/cappuccino): ")
+        if choice == "off":
+            is_working = False
+        elif choice == "report":
+            print_report()
+        else:
+            if choice != "espresso" and choice != "latte" and choice != "cappuccino":
+                print("Invalid input.")
+                continue
+            drink = MENU[choice]
+            if can_made(drink["ingredients"]):
+                print(f"The price of {choice}is {drink['cost']}$")
+                payment = count_coins()
+                if is_transaction_successful(payment, drink["cost"]):
+                    make_coffee(choice, drink["ingredients"])
+
+work()
