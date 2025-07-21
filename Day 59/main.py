e6d4384398150860dc4779f9701b89824740fc28
SyAndrey <69247757+SyAndrey006@users.noreply.github.com>
2025-07-21 13:32:26 +0200

    Add files via upload

---
+++ Day 59/main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+import requests
+
+posts = requests.get("https://api.npoint.io/b9d21b4c923db38e247c").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+
+
+#set FLASK_APP=main.py
+#python -m flask run
