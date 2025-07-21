1134732e7b717c2463cfb826ec8c028f4e49a831
SyAndrey <69247757+SyAndrey006@users.noreply.github.com>
2025-07-21 12:56:53 +0200

    main.py

--- Day 32/Auto Email Sender/main.py
+++ Day 32/Auto Email Sender/main.py
@@ -3,8 +3,8 @@ import pandas
 import random
 import smtplib
 
-MY_EMAIL = "andrewsynelnyk@gmail.com"
-MY_PASSWORD = "Andrey1507"
+MY_EMAIL = ""
+MY_PASSWORD = ""
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
