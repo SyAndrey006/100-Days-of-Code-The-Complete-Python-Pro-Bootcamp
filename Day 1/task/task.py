6a4d8256c974b2ce169de24c79122c2ec2e7413b
SyAndrey <69247757+SyAndrey006@users.noreply.github.com>
2025-07-21 12:31:16 +0200

    Add files via upload

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Band Name Generator Project")
+city_you_live = input("What's the name of the city you grow up in ?\n")
+name_of_your_pet = input("What's your pet's name ?\n")
+print("Your band name could be " + city_you_live + name_of_your_pet)
---
+++ Day 3/If Else/task.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height>=120:
+    print("You are allowed to ride the rollercoaster")
+else:
+    print("You are not allowed to ride the rollercoaster")
